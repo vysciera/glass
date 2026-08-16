298a5510f700acd83309821c3704abfec30d7aa5
dirac <hobbiathan@gmail.com>
2026-08-16 08:18:34 -0600

    func Square(), board Set()

--- chess/board.go
+++ chess/board.go
@@ -1,3 +1,11 @@
 package chess
 
 type Board [64]Piece
+
+func Square(file, rank int) int {
+	return rank*8 + file
+}
+
+func (b *Board) Set(file, rank int, piece Piece) {
+	b[Square(file, rank)] = piece
+}
