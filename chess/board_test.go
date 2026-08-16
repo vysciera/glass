2d6f948d678019e0098b9cc9baf5daf49574e471
dirac <hobbiathan@gmail.com>
2026-08-16 09:05:40 -0600

    test refactor, TDD approach

--- chess/board_test.go
+++ chess/board_test.go
@@ -4,27 +4,50 @@ import "testing"
 
 func TestSquare(t *testing.T) {
 	tests := []struct {
+		name string
 		file int
 		rank int
 		want int
 	}{
-		{file: 0, rank: 0, want: 0},
-		{file: 4, rank: 0, want: 4},
-		{file: 0, rank: 1, want: 8},
-		{file: 7, rank: 7, want: 63},
+		{
+			name: "a1 is square 0",
+			file: 0,
+			rank: 0,
+			want: 0,
+		},
+		{
+			name: "e1 is square 4",
+			file: 4,
+			rank: 0,
+			want: 4,
+		},
+		{
+			name: "a2 is square 8",
+			file: 0,
+			rank: 1,
+			want: 8,
+		},
+		{
+			name: "h8 is square 63",
+			file: 7,
+			rank: 7,
+			want: 63,
+		},
 	}
 
 	for _, tt := range tests {
-		got := Square(tt.file, tt.rank)
+		t.Run(tt.name, func(t *testing.T) {
+			got := Square(tt.file, tt.rank)
 
-		if got != tt.want {
-			t.Errorf(
-				"Square(%d, %d) = %d; want %d",
-				tt.file,
-				tt.rank,
-				got,
-				tt.want,
-			)
-		}
+			if got != tt.want {
+				t.Errorf(
+					"Square(%d, %d) = %d; want %d",
+					tt.file,
+					tt.rank,
+					got,
+					tt.want,
+				)
+			}
+		})
 	}
 }
