2bcd4105dad640f39c0a599289fb4d6d90b9fdf8
dirac <hobbiathan@gmail.com>
2026-08-17 15:45:09 -0600

    expand TestPieceSymbol tests

--- chess/piece_test.go
+++ chess/piece_test.go
@@ -31,6 +31,54 @@ func TestPieceSymbol(t *testing.T) {
 			},
 			want: ".",
 		},
+		{
+			name: "white queen",
+			piece: Piece{
+				Type:  Queen,
+				Color: White,
+			},
+			want: "Q",
+		},
+		{
+			name: "black queen",
+			piece: Piece{
+				Type:  Queen,
+				Color: Black,
+			},
+			want: "q",
+		},
+		{
+			name: "white knight",
+			piece: Piece{
+				Type:  Knight,
+				Color: White,
+			},
+			want: "N",
+		},
+		{
+			name: "black knight",
+			piece: Piece{
+				Type:  Knight,
+				Color: Black,
+			},
+			want: "n",
+		},
+		{
+			name: "white pawn",
+			piece: Piece{
+				Type:  Pawn,
+				Color: White,
+			},
+			want: "P",
+		},
+		{
+			name: "black pawn",
+			piece: Piece{
+				Type:  Pawn,
+				Color: Black,
+			},
+			want: "p",
+		},
 	}
 
 	for _, tt := range tests {
@@ -38,7 +86,7 @@ func TestPieceSymbol(t *testing.T) {
 			got := tt.piece.Symbol()
 
 			if got != tt.want {
-				t.Errorf("Symbol() = &q; want %q", got, tt.want)
+				t.Errorf("Symbol() = %q; want %q", got, tt.name)
 			}
 		})
 	}
