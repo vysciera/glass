ec975f185bb39683bf43c7cef89a93fcf27704cd
dirac <hobbiathan@gmail.com>
2026-08-17 21:15:18 -0600

    Board.String()

--- chess/board.go
+++ chess/board.go
@@ -1,6 +1,8 @@
 // Package chess manages board position & engine logic
 package chess
 
+import "strings"
+
 type Board [64]Piece
 
 func Square(file, rank int) int {
@@ -50,4 +52,26 @@ func NewStartingBoard() Board {
 	}
 
 	return board
+}
+
+func (b Board) String() string {
+	var out strings.Builder
+
+	for rank := 7; rank >= 0; rank-- {
+		for file := 0; file < 8; file++ {
+			symbol := b[Square(file, rank)].Symbol()
+
+			if file != 7 {
+				symbol += " "
+			}
+
+			out.WriteString(symbol)
+		}
+
+		if rank != 0 {
+			out.WriteString("\n")
+		}
+	}
+
+	return out.String()
 }
